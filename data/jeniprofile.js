95387fad4ca43b7f645d314d1f49d00dbdacc809
Jenitta-Raja-Kumari <87815186+Jenitta-Raja-Kumari@users.noreply.github.com>
2022-04-07 00:00:05 +0530

    Initial upload

---
+++ data/jeniprofile.js
@@ -0,0 +1,134 @@
+const profileData = {
+  title: 'Profile',
+  name: 'JENITTA RAJA KUMARI P ',
+  sub_title: 'SOFTWARE DEVELOPER',
+  about: {
+    objective:'To secure a challenging position in a reputed organization where I can learn new skills, expand my knowledge and my learnings.To get an opportunity where I can make the best of my potential and contibute to the organizations growth.',
+    contact: {
+      email: 'jenijeryl86@gmail.com',
+      phone: '9150474861',
+      address: '.....'
+    }
+  },
+  links: [
+    { title: 'Github', src: 'https://github.com/Jenitta-Raja-Kumari' },
+    { title: 'LinkedIn', src: 'https://www.linkedin.com/in/jenitta-raja-kumari-065aba209/' }
+  ],
+  education: [
+    {
+      alma: 'St Josephs Institute Of Technology,OMR, Chennai',
+      duration: 'Jul 2019 - present',
+      std: 'B.Tech IT',
+      score: '8.67 (till 4th sem)'
+    },
+    {
+      alma: 'Little Flower Girls Higher Secondary School',
+      duration: 'Jun 2018 - Mar 2019',
+      std: '-12th Grade',
+      score: '80%'
+    },
+    {
+      alma: 'Little Flower Girls Higher Secondary School',
+      duration: 'Jun 2016 - Mar 2017',
+      std: '-10th Grade',
+      score: '97.4%'
+    }
+
+  ],
+  skills: [
+    {
+      
+      topics: ['C Programming']
+    },
+    {
+     
+      topics: ['JAVA'
+      ]
+    },
+    {
+      
+      topics: ['HTML']
+    },
+    {
+      
+      topics:['CSS']
+    },
+    {
+     
+      topics:['JavaScript(basics)']
+    },
+    {
+      
+      topics:['python(basics)']
+    }
+  ],
+  projects: [
+    {
+      title: 'SMAIL Software',
+      duration: ' May 2021 - Jun 2021',
+      link: 'https://github.com/Jenitta-Raja-Kumari/SMAIL-Software.git',
+
+      desc: 'This is a mail software that is highly secured with End-to-End Encryption Algorithm. Hence called as SECURE MAIL(SMAIL)'
+      
+    },
+    {
+      title: 'PARKOSCOPE',
+      duration: 'NOV 2021 - DEC 2021',
+      link: 'https://github.com/Jenitta-Raja-Kumari/Parkoscope',
+      desc: 'This website helps the user to find the parking slots'
+    },
+    {
+      title: 'STEPCOUNTER',
+      duration: 'Sep 2021 - Oct 2021',
+      link: 'https://github.com/Jenitta-Raja-Kumari/step-counter',
+      desc: ''
+    }
+  ],
+  internship: [
+    {
+      organization: 'TCR INNOVATION',
+
+      date: 'Oct 2021 - Dec 2021',
+      details: [
+        'Front-End developer.'
+      ]
+    },
+    {
+      organization: 'EXPOSYS DATA LAB',
+     
+      date:'May 2021 - Jun 2021' ,
+      details: [
+        'Software development.'
+      ]
+    },
+    {
+      organization: 'MerJer Softech',
+
+      date: 'Nov 20',
+      details: [
+        'Hero Honda Sales and Service Management.'
+      ]
+    }
+  ],
+  certifications: [
+    {
+      title:'SHAPEAI',
+      desc: 'Python and Data Analytics Bootcamp',
+      date: 'Jun 2021'
+    },
+    {
+      title:'GUVI Geek Networks, IITM Research Park',
+      desc: 'AI-For-India Guinness World Record',
+      date: ' Apr 2021'
+    }
+  ],
+
+declaration:
+    {
+      desc:'I hereby declare that the above particulars of facts and information stated are true, correct and complete to the best of my belief and knowledge.',
+      place:'Place : Chennai'
+     
+  
+ }
+  
+ };
